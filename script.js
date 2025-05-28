dd7136721506c79181a7bdd7e4467a1479cf1115
Yusuf-4hmed <yusufahmed0625@gmail.com>
2025-05-28 22:26:00 +0100

    mobile nav now functional

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// MOBILE NAV BAR
+
+const navMenuButton = document.getElementById("nav-menu-button");
+const mobileNav = document.getElementById("mobile-nav")
+
+navMenuButton.addEventListener("click", () => {
+    if (mobileNav.style.display === "none" || mobileNav.style.display === "") {
+        mobileNav.style.display = "block"
+    } else {
+        mobileNav.style.display = "none"
+    }
+})
+
+// STUDENT PASS GALLERY
+
 const rightButton = document.getElementById("right-button");
 const leftButton = document.getElementById("left-button")
 const studentPassContainers = document.querySelectorAll(".student-pass-container")
